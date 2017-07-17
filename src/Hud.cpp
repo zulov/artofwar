a6c8ff8047c1101b367874f8b221f2ead6677db9
Tomasz Gajda <zulov92@gmail.com>
2017-07-17 23:20:07 +0200

    przyklad z parserem

--- src/Hud.cpp
+++ src/Hud.cpp
@@ -1,7 +1,7 @@
 #include "Hud.h"
 #include "Game.h"
 #include <regex>
-
+#include <exprtk/exprtk.hpp>
 
 void Hud::replaceVariables(XMLFile* xmlFile, db_hud_size* hudSize) {
 	auto windowsStyleString = xmlFile->ToString();
@@ -43,6 +43,31 @@ Hud::Hud() {
 
 	cursor->SetPosition(Game::get()->getGraphics()->GetWidth() / 2, Game::get()->getGraphics()->GetHeight() / 2);
 	selectedHudPanel = new SelectedHudPanel(style, hudSize, font, selectedInfoWindow);
+
+	{
+		typedef exprtk::symbol_table<double> symbol_table_t;
+		typedef exprtk::expression<double>     expression_t;
+		typedef exprtk::parser<double>             parser_t;
+		std::string expression_string = "clamp(-1.0,sin(2 * pi * x) + cos(x / 2 * pi),+1.0)";
+
+		double x;
+
+		symbol_table_t symbol_table;
+		symbol_table.add_variable("x", x);
+		symbol_table.add_constants();
+
+		expression_t expression;
+		expression.register_symbol_table(symbol_table);
+
+		parser_t parser;
+		parser.compile(expression_string, expression);
+
+		for (x = double(-5); x <= double(+5); x += double(0.001))
+		{
+			double y = expression.value();
+			printf("%19.15f\t%19.15f\n", x, y);
+		}
+	}
 }
 
 
