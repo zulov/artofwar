0f65ef40f800eed11f645b5592e9a77cf77f44d1
Tomasz Gajda <zulov92@gmail.com>
2020-03-05 11:44:58 +0100

    init tab

--- src/simulation/env/CloseIndexProvider.cpp
+++ src/simulation/env/CloseIndexProvider.cpp
@@ -2,18 +2,15 @@
 
 CloseIndexProvider::CloseIndexProvider(short res)
 	: resolution(res), closeindexes{
-		  {1,res, res + 1},
-		  {-1,1, res - 1, res, res + 1},
-		  {1},
-		  {1},
+		  {1, res, res + 1},
+		  {-1, 1, res - 1, res, res + 1},
+		  {-1, res - 1, res},
+		  {-res, -res + 1, 1, res, res + 1},
 		  {-res - 1, -res, -res + 1, -1, 1, res - 1, res, res + 1},
-		  {1},
-		  {1},
-		  {1},
-		  {1}
-	  } {
-}
+		  {-res - 1, -res, -1, res - 1, res},
+		  {-res, -res + 1, 1},
+		  {-res - 1, -res, -res + 1, -1, 1},
+		  {-res - 1, -res, -1}
+	  } {}
 
-std::vector<short>& CloseIndexProvider::get(int current) {
-
-}
+std::vector<short>& CloseIndexProvider::get(int current) {}
