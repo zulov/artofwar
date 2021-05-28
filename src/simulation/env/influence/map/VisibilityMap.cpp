5d285aa3958f52549a6348cf56fd56af2ed52769
Tomasz Gajda <zulov92@gmail.com>
2021-05-28 08:52:14 +0200

    visibilityMap

---
+++ src/simulation/env/influence/map/VisibilityMap.cpp
@@ -0,0 +1,67 @@
+#include "VisibilityMap.h"
+
+#include "objects/Physical.h"
+
+VisibilityMap::VisibilityMap(unsigned short resolution, float size, float valueThresholdDebug): InfluenceMap(
+	resolution, size, valueThresholdDebug) {
+	values = new char[arraySize];
+	std::fill_n(values, arraySize, -1);
+}
+
+VisibilityMap::~VisibilityMap() {
+	delete[] values;
+}
+
+void VisibilityMap::update(Physical* thing, float value) {
+	char sRadius = thing->getSightRadius();
+	auto index = calculator->indexFromPosition(thing->getPosition());
+	auto [centerX,centerZ] = calculator->getIndexes(index);
+	const auto minI = calculator->getValid(centerX - sRadius);
+	const auto maxI = calculator->getValid(centerX + sRadius);
+
+	const auto minJ = calculator->getValid(centerZ - sRadius);
+	const auto maxJ = calculator->getValid(centerZ + sRadius);
+}
+
+
+void VisibilityMap::reset() {
+	for (int i = 0; i < arraySize; ++i) {
+		if (values[i] == 1) {
+			values[i] = 1;
+		}
+	}
+}
+
+char VisibilityMap::getValueAt(const Urho3D::Vector2& pos) const {
+	auto index = calculator->indexFromPosition(pos);
+	return getValueAt(index);
+}
+
+float VisibilityMap::getValueAsPercent(const Urho3D::Vector2& pos) const {
+	const float diff = max - min;
+	if (diff != 0.f) {
+		return (getValueAt(pos) - min) / diff;
+	}
+	return 0.5f;
+}
+
+float VisibilityMap::getValueAsPercent(const int index) const {
+	const float diff = max - min;
+	if (diff != 0.f) {
+		return (getValueAt(index) - min) / diff;
+	}
+	return 0.5f;
+}
+
+float VisibilityMap::getValueAt(int index) const {
+	return values[index];
+}
+
+void VisibilityMap::computeMinMax() {
+	if (!minMaxInited) {
+		const auto [minIdx, maxIdx] = std::minmax_element(values, values + arraySize);
+		min = *minIdx;
+		max = *maxIdx;
+		minMaxInited = true;
+	}
+}
---
+++ src/simulation/env/influence/map/VisibilityMap.h
@@ -0,0 +1,21 @@
+#pragma once
+#include "InfluenceMap.h"
+#include "InfluenceMapInt.h"
+
+class VisibilityMap : public InfluenceMap {
+public:
+	VisibilityMap(unsigned short resolution, float size, float valueThresholdDebug);
+	~VisibilityMap() override;
+
+	void update(Physical* thing, float value = 1.f) override;
+	void updateInt(Physical* thing, int value = 1) override;
+	void updateInt(int index, int value = 1) const;
+	void reset() override;
+	char getValueAt(const Urho3D::Vector2& pos) const;
+	float getValueAsPercent(const Urho3D::Vector2& pos) const override;
+	float getValueAsPercent(const int index) const override;
+	float getValueAt(int index) const override;
+	void computeMinMax() override;
+private:
+	char* values;
+};
