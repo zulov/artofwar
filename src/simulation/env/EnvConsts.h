2efb484a081649785eb2e9b668803e2d7eb59c90
Tomasz Gajda <zulov92@gmail.com>
2021-07-06 12:01:20 +0200

    bigger grid resource

--- src/simulation/env/EnvConsts.h
+++ src/simulation/env/EnvConsts.h
@@ -5,7 +5,7 @@ constexpr float BUCKET_GRID_FIELD_SIZE = 2.f;
 
 constexpr float BUCKET_GRID_FIELD_SIZE_BUILD = 8.f;
 constexpr float BUCKET_GRID_FIELD_SIZE_ENEMY = 16.f;
-constexpr float BUCKET_GRID_FIELD_SIZE_RESOURCE = 8.f;
+constexpr float BUCKET_GRID_FIELD_SIZE_RESOURCE = 16.f;
 
 constexpr inline InfluenceDataType ENV_TO_INF_MAP[magic_enum::enum_count<EnvironmentDebugMode>()] = {
 	InfluenceDataType::NONE, InfluenceDataType::NONE, InfluenceDataType::UNITS_NUMBER_PER_PLAYER,
