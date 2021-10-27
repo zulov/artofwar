20fc71b9dec777a9b2867b7b2421de09d281080a
Tomasz Gajda <zulov92@gmail.com>
2021-10-27 23:00:54 +0200

    perinstance

--- src/objects/queue/QueueUtils.h
+++ src/objects/queue/QueueUtils.h
@@ -6,14 +6,13 @@
 inline float getSecToComplete(QueueActionType type, short id, int level) {
 	switch (type) {
 	case QueueActionType::UNIT_CREATE:
-		
 		return Game::getDatabase()->getUnit(id)->getLevel(level).value()->buildTime;
 	case QueueActionType::BUILDING_CREATE:
 		return Game::getDatabase()->getBuilding(id)->getLevel(level).value()->buildTime;
 	case QueueActionType::UNIT_LEVEL:
 		return Game::getDatabase()->getUnit(id)->getLevel(level).value()->upgradeTime; //TODO BUG value
 	case QueueActionType::BUILDING_LEVEL:
-		return 15;
+		return Game::getDatabase()->getBuilding(id)->getLevel(level).value()->upgradeTime;
 	default:
 		return 1;
 	}
@@ -23,7 +22,7 @@ inline float getSecPerInstance(QueueActionType type, short id, int level) {
 	//TODO performance przerobic na tablice
 	switch (type) {
 	case QueueActionType::UNIT_CREATE:
-		return 1;
+		return 30;
 	case QueueActionType::BUILDING_CREATE:
 	case QueueActionType::UNIT_LEVEL:
 	case QueueActionType::BUILDING_LEVEL:
