f77696f2ac8ece42c0724417b2552f357de43ea8
Tomasz Gajda <zulov92@gmail.com>
2024-10-08 22:59:21 +0200

    clean

--- game/src/player/ai/MetricDefinitions.h
+++ game/src/player/ai/MetricDefinitions.h
@@ -46,8 +46,8 @@ constexpr inline struct MetricDefinitions {
 	}
 
 	const std::vector<float> getBuildingNorm(std::span<AiBuildingMetric> span,
-	                           const std::function<float(const AiBuildingMetric&)>& getWeight,
-	                           db_building* building, db_building_level* level) const {
+	                                         const std::function<float(const AiBuildingMetric&)>& getWeight,
+	                                         db_building* building, db_building_level* level) const {
 		std::vector<float> result;
 		result.reserve(span.size());
 		for (auto const& v : span) {
@@ -57,11 +57,12 @@ constexpr inline struct MetricDefinitions {
 	}
 
 	const std::vector<float> getBuildingNorm(db_building* building, db_building_level* level) const {
-		return getBuildingNorm(buildingInputSpan, [](const AiBuildingMetric& m){ return m.weight; }, building, level);
+		return getBuildingNorm(buildingInputSpan, [](const AiBuildingMetric& m) { return m.weight; }, building, level);
 	}
 
 	const std::vector<float> getBuildingNormForSum(db_building* building, db_building_level* level) const {
-		return getBuildingNorm(buildingInputSpan, [](const AiBuildingMetric& m){ return m.weightForSum; }, building, level);
+		return getBuildingNorm(buildingInputSpan, [](const AiBuildingMetric& m) { return m.weightForSum; }, building,
+		                       level);
 	}
 
 	// const std::vector<float> getResourceNorm(Resources& resources, Possession& possession) const {
@@ -114,62 +115,62 @@ constexpr inline struct MetricDefinitions {
 	const std::span<unsigned char> getResWithoutBonusIdxs() const { return aiResWithoutBonusIdxsSpan; }
 
 
-	static inline AiUnitMetric aiUnitMetric[] = {//db_unit* u, db_unit_level* l
-		{[](db_unit* u, db_unit_level* l) -> float{ return u->getSumCost(); }, 400, UNITS_SUM_X},
+	static inline AiUnitMetric aiUnitMetric[] = { //db_unit* u, db_unit_level* l
+		{[](auto u, auto l) -> float { return u->getSumCost(); }, 400, UNITS_SUM_X},
 		//TODO czy grupowe ma sens?
-		{[](db_unit* u, db_unit_level* l) -> float{ return l->maxHp; }, 300, UNITS_SUM_X},
-		{[](db_unit* u, db_unit_level* l) -> float{ return l->armor; }, 1, UNITS_SUM_X},
-		{[](db_unit* u, db_unit_level* l) -> float{ return l->sightRadius; }, 20, UNITS_SUM_X},
-
-		{[](db_unit* u, db_unit_level* l) -> float{ return l->collect; }, 1, UNITS_SUM_X},
-		{[](db_unit* u, db_unit_level* l) -> float{ return l->attack; }, 10, UNITS_SUM_X}, //index 5
-		{[](db_unit* u, db_unit_level* l) -> float{ return l->attackReload; }, 200, UNITS_SUM_X},
-		{[](db_unit* u, db_unit_level* l) -> float{ return l->attackRange; }, 20, UNITS_SUM_X},
-
-		{[](db_unit* u, db_unit_level* l) -> float{ return u->typeInfantry; }, 1, UNITS_SUM_X}, //8
-		{[](db_unit* u, db_unit_level* l) -> float{ return u->typeRange; }, 1, UNITS_SUM_X},
-		{[](db_unit* u, db_unit_level* l) -> float{ return u->typeCalvary; }, 1, UNITS_SUM_X},
-		{[](db_unit* u, db_unit_level* l) -> float{ return u->typeWorker; }, 1, UNITS_SUM_X},
-		{[](db_unit* u, db_unit_level* l) -> float{ return u->typeSpecial; }, 1, UNITS_SUM_X},
-		{[](db_unit* u, db_unit_level* l) -> float{ return u->typeMelee; }, 1, UNITS_SUM_X},
-		{[](db_unit* u, db_unit_level* l) -> float{ return u->typeHeavy; }, 1, UNITS_SUM_X},
-		{[](db_unit* u, db_unit_level* l) -> float{ return u->typeLight; }, 1, UNITS_SUM_X}, //15
-
-		{[](db_unit* u, db_unit_level* l) -> float{ return l->bonusInfantry; }, 1, UNITS_SUM_X},
-		{[](db_unit* u, db_unit_level* l) -> float{ return l->bonusRange; }, 1, UNITS_SUM_X},
-		{[](db_unit* u, db_unit_level* l) -> float{ return l->bonusCalvary; }, 1, UNITS_SUM_X},
-		{[](db_unit* u, db_unit_level* l) -> float{ return l->bonusWorker; }, 1, UNITS_SUM_X},
-		{[](db_unit* u, db_unit_level* l) -> float{ return l->bonusSpecial; }, 1, UNITS_SUM_X},
-		{[](db_unit* u, db_unit_level* l) -> float{ return l->bonusMelee; }, 1, UNITS_SUM_X},
-		{[](db_unit* u, db_unit_level* l) -> float{ return l->bonusHeavy; }, 1, UNITS_SUM_X},
-		{[](db_unit* u, db_unit_level* l) -> float{ return l->bonusLight; }, 1, UNITS_SUM_X},
+		{[](auto u, auto l) -> float { return l->maxHp; }, 300, UNITS_SUM_X},
+		{[](auto u, auto l) -> float { return l->armor; }, 1, UNITS_SUM_X},
+		{[](auto u, auto l) -> float { return l->sightRadius; }, 20, UNITS_SUM_X},
+					
+		{[](auto u, auto l) -> float { return l->collect; }, 1, UNITS_SUM_X},
+		{[](auto u, auto l) -> float { return l->attack; }, 10, UNITS_SUM_X}, //index 5
+		{[](auto u, auto l) -> float { return l->attackReload; }, 200, UNITS_SUM_X},
+		{[](auto u, auto l) -> float { return l->attackRange; }, 20, UNITS_SUM_X},
+					
+		{[](auto u, auto l) -> float { return u->typeInfantry; }, 1, UNITS_SUM_X}, //8
+		{[](auto u, auto l) -> float { return u->typeRange; }, 1, UNITS_SUM_X},
+		{[](auto u, auto l) -> float { return u->typeCalvary; }, 1, UNITS_SUM_X},
+		{[](auto u, auto l) -> float { return u->typeWorker; }, 1, UNITS_SUM_X},
+		{[](auto u, auto l) -> float { return u->typeSpecial; }, 1, UNITS_SUM_X},
+		{[](auto u, auto l) -> float { return u->typeMelee; }, 1, UNITS_SUM_X},
+		{[](auto u, auto l) -> float { return u->typeHeavy; }, 1, UNITS_SUM_X},
+		{[](auto u, auto l) -> float { return u->typeLight; }, 1, UNITS_SUM_X}, //15
+					
+		{[](auto u, auto l) -> float { return l->bonusInfantry; }, 1, UNITS_SUM_X},
+		{[](auto u, auto l) -> float { return l->bonusRange; }, 1, UNITS_SUM_X},
+		{[](auto u, auto l) -> float { return l->bonusCalvary; }, 1, UNITS_SUM_X},
+		{[](auto u, auto l) -> float { return l->bonusWorker; }, 1, UNITS_SUM_X},
+		{[](auto u, auto l) -> float { return l->bonusSpecial; }, 1, UNITS_SUM_X},
+		{[](auto u, auto l) -> float { return l->bonusMelee; }, 1, UNITS_SUM_X},
+		{[](auto u, auto l) -> float { return l->bonusHeavy; }, 1, UNITS_SUM_X},
+		{[](auto u, auto l) -> float { return l->bonusLight; }, 1, UNITS_SUM_X},
 	};
 
-	static inline AiBuildingMetric aiBuildingMetric[] = {//db_building* b, db_building_level* l
-		{[](auto b, auto l) -> float{ return b->getSumCost(); }, 400, BUILDINGS_SUM_X},
-		{[](auto b, auto l) -> float{ return l->maxHp; }, 500, BUILDINGS_SUM_X},
-		{[](auto b, auto l) -> float{ return l->armor; }, 1, BUILDINGS_SUM_X},
-		{[](auto b, auto l) -> float{ return l->sightRadius; }, 50, BUILDINGS_SUM_X},
+	static inline AiBuildingMetric aiBuildingMetric[] = { //db_building* b, db_building_level* l
+		{[](auto b, auto l) -> float { return b->getSumCost(); }, 400, BUILDINGS_SUM_X},
+		{[](auto b, auto l) -> float { return l->maxHp; }, 500, BUILDINGS_SUM_X},
+		{[](auto b, auto l) -> float { return l->armor; }, 1, BUILDINGS_SUM_X},
+		{[](auto b, auto l) -> float { return l->sightRadius; }, 50, BUILDINGS_SUM_X},
 
-		{[](auto b, auto l) -> float{ return l->collect; }, 2, BUILDINGS_SUM_X},
-		{[](auto b, auto l) -> float{ return l->attack; }, 20, BUILDINGS_SUM_X}, //5
-		{[](auto b, auto l) -> float{ return l->attackReload; }, 200, BUILDINGS_SUM_X},
-		{[](auto b, auto l) -> float{ return l->attackRange; }, 20, BUILDINGS_SUM_X},
-		{[](auto b, auto l) -> float{ return l->resourceRange; }, 20, BUILDINGS_SUM_X},
+		{[](auto b, auto l) -> float { return l->collect; }, 2, BUILDINGS_SUM_X},
+		{[](auto b, auto l) -> float { return l->attack; }, 20, BUILDINGS_SUM_X}, //5
+		{[](auto b, auto l) -> float { return l->attackReload; }, 200, BUILDINGS_SUM_X},
+		{[](auto b, auto l) -> float { return l->attackRange; }, 20, BUILDINGS_SUM_X},
+		{[](auto b, auto l) -> float { return l->resourceRange; }, 20, BUILDINGS_SUM_X},
 		//TODO stad duzo wyrzuciæ
 
-		{[](auto b, auto l) -> float{ return b->typeCenter; }, 1, BUILDINGS_SUM_X}, //9
-		{[](auto b, auto l) -> float{ return b->typeHome; }, 1, BUILDINGS_SUM_X},
-		{[](auto b, auto l) -> float{ return b->typeDefence; }, 1, BUILDINGS_SUM_X},
-		{[](auto b, auto l) -> float{ return b->typeResourceFood; }, 1, BUILDINGS_SUM_X}, //12
-		{[](auto b, auto l) -> float{ return b->typeResourceWood; }, 1, BUILDINGS_SUM_X},
-		{[](auto b, auto l) -> float{ return b->typeResourceStone; }, 1, BUILDINGS_SUM_X},
-		{[](auto b, auto l) -> float{ return b->typeResourceGold; }, 1, BUILDINGS_SUM_X}, //15
-		{[](auto b, auto l) -> float{ return b->typeTechBlacksmith; }, 1, BUILDINGS_SUM_X}, //16
-		{[](auto b, auto l) -> float{ return b->typeTechUniversity; }, 1, BUILDINGS_SUM_X},
-		{[](auto b, auto l) -> float{ return b->typeUnitBarracks; }, 1, BUILDINGS_SUM_X}, //18
-		{[](auto b, auto l) -> float{ return b->typeUnitRange; }, 1, BUILDINGS_SUM_X},
-		{[](auto b, auto l) -> float{ return b->typeUnitCavalry; }, 1, BUILDINGS_SUM_X},
+		{[](auto b, auto l) -> float { return b->typeCenter; }, 1, BUILDINGS_SUM_X}, //9
+		{[](auto b, auto l) -> float { return b->typeHome; }, 1, BUILDINGS_SUM_X},
+		{[](auto b, auto l) -> float { return b->typeDefence; }, 1, BUILDINGS_SUM_X},
+		{[](auto b, auto l) -> float { return b->typeResourceFood; }, 1, BUILDINGS_SUM_X}, //12
+		{[](auto b, auto l) -> float { return b->typeResourceWood; }, 1, BUILDINGS_SUM_X},
+		{[](auto b, auto l) -> float { return b->typeResourceStone; }, 1, BUILDINGS_SUM_X},
+		{[](auto b, auto l) -> float { return b->typeResourceGold; }, 1, BUILDINGS_SUM_X}, //15
+		{[](auto b, auto l) -> float { return b->typeTechBlacksmith; }, 1, BUILDINGS_SUM_X}, //16
+		{[](auto b, auto l) -> float { return b->typeTechUniversity; }, 1, BUILDINGS_SUM_X},
+		{[](auto b, auto l) -> float { return b->typeUnitBarracks; }, 1, BUILDINGS_SUM_X}, //18
+		{[](auto b, auto l) -> float { return b->typeUnitRange; }, 1, BUILDINGS_SUM_X},
+		{[](auto b, auto l) -> float { return b->typeUnitCavalry; }, 1, BUILDINGS_SUM_X},
 	};
 	//TODO improve nie indeksy ale enumy?
 	static inline unsigned char aiUnitsTypesIdxs[] = {8, 9, 10, 11, 12, 12, 13, 14, 15};
@@ -185,101 +186,101 @@ constexpr inline struct MetricDefinitions {
 	static inline unsigned char aiResWithoutBonusIdxs[] = {10, 11, 12, 13};
 
 	//TODO moze to zwracac od razy przedzia³em jakos
-	static inline AiResourceMetric aiResourceMetric[] = {//Resources* r, Possession* p
-		{[](Resources* r, Possession* p) -> float{ return r->getGatherSpeeds()[0]; }, 10},
-		{[](Resources* r, Possession* p) -> float{ return r->getGatherSpeeds()[1]; }, 10},
-		{[](Resources* r, Possession* p) -> float{ return r->getGatherSpeeds()[2]; }, 10},
-		{[](Resources* r, Possession* p) -> float{ return r->getGatherSpeeds()[3]; }, 10},
-
-		{[](Resources* r, Possession* p) -> float{ return r->getValues()[0]; }, 1000},
-		{[](Resources* r, Possession* p) -> float{ return r->getValues()[1]; }, 1000},
-		{[](Resources* r, Possession* p) -> float{ return r->getValues()[2]; }, 1000},
-		{[](Resources* r, Possession* p) -> float{ return r->getValues()[3]; }, 1000},
-
-		{[](Resources* r, Possession* p) -> float{ return p->getFreeWorkersNumber(); }, 100},
-		{[](Resources* r, Possession* p) -> float{ return p->getWorkersNumber(); }, 100},
-		{[](Resources* r, Possession* p) -> float{ return p->getResWithOutBonus()[0]; }, 20}, //10
-		{[](Resources* r, Possession* p) -> float{ return p->getResWithOutBonus()[1]; }, 20},
-		{[](Resources* r, Possession* p) -> float{ return p->getResWithOutBonus()[2]; }, 20},
-		{[](Resources* r, Possession* p) -> float{ return p->getResWithOutBonus()[3]; }, 20},
+	static inline AiResourceMetric aiResourceMetric[] = { //Resources* r, Possession* p
+		{[](auto r, auto p) -> float { return r->getGatherSpeeds()[0]; }, 10},
+		{[](auto r, auto p) -> float { return r->getGatherSpeeds()[1]; }, 10},
+		{[](auto r, auto p) -> float { return r->getGatherSpeeds()[2]; }, 10},
+		{[](auto r, auto p) -> float { return r->getGatherSpeeds()[3]; }, 10},
+					
+		{[](auto r, auto p) -> float { return r->getValues()[0]; }, 1000},
+		{[](auto r, auto p) -> float { return r->getValues()[1]; }, 1000},
+		{[](auto r, auto p) -> float { return r->getValues()[2]; }, 1000},
+		{[](auto r, auto p) -> float { return r->getValues()[3]; }, 1000},
+					
+		{[](auto r, auto p) -> float { return p->getFreeWorkersNumber(); }, 100},
+		{[](auto r, auto p) -> float { return p->getWorkersNumber(); }, 100},
+		{[](auto r, auto p) -> float { return p->getResWithOutBonus()[0]; }, 20}, //10
+		{[](auto r, auto p) -> float { return p->getResWithOutBonus()[1]; }, 20},
+		{[](auto r, auto p) -> float { return p->getResWithOutBonus()[2]; }, 20},
+		{[](auto r, auto p) -> float { return p->getResWithOutBonus()[3]; }, 20},
 	};
 
-	static inline AiPlayerMetric aiBasicMetric[] = {
-		{[](Player* one, Player* two) -> float{ return one->getScore(); }, 1000},
-		{[](Player* one, Player* two) -> float{ return one->getPossession()->getUnitsNumber(); }, 200},
-		{[](Player* one, Player* two) -> float{ return one->getPossession()->getBuildingsNumber(); }, 50},
+	static inline AiPlayerMetric aiBasicMetric[] = {//Player* p1, Player* p2
+		{[](auto p1, auto p2) -> float { return p1->getScore(); }, 1000},
+		{[](auto p1, auto p2) -> float { return p1->getPossession()->getUnitsNumber(); }, 200},
+		{[](auto p1, auto p2) -> float { return p1->getPossession()->getBuildingsNumber(); }, 50},
 
-		{[](Player* one, Player* two) -> float{ return two->getScore(); }, 1000}
+		{[](auto p1, auto p2) -> float { return p2->getScore(); }, 1000}
 	};
 
-	static inline AiPlayerMetric aiAttackOrDefence[] = {
-		{[](Player* p1, Player* p2) -> float{ return p1->getPossession()->getAttackSum(); }, 1000},
-		{[](Player* p1, Player* p2) -> float{ return p1->getPossession()->getDefenceAttackSum(); }, 100},
+	static inline AiPlayerMetric aiAttackOrDefence[] = { //Player* p1, Player* p2
+		{[](auto p1, auto p2) -> float { return p1->getPossession()->getAttackSum(); }, 1000},
+		{[](auto p1, auto p2) -> float { return p1->getPossession()->getDefenceAttackSum(); }, 100},
 		{
-			[](Player* p1, Player* p2) -> float{
+			[](auto p1, auto p2) -> float {
 				return Game::getEnvironment()->getDiffOfCenters(CenterType::ARMY, p1->getId(), CenterType::BUILDING,
 				                                                p1->getId(), 0.f);
 			}
 		},
 		{
-			[](Player* p1, Player* p2) -> float{
+			[](auto p1, auto p2) -> float {
 				return Game::getEnvironment()->getDiffOfCenters(CenterType::ARMY, p1->getId(), CenterType::BUILDING,
 				                                                p2->getId(), 1.f);
 			}
 		},
 		{
-			[](Player* p1, Player* p2) -> float{
+			[](auto p1, auto p2) -> float {
 				return Game::getEnvironment()->getDiffOfCenters(CenterType::ARMY, p2->getId(), CenterType::BUILDING,
 				                                                p1->getId(), 1.f);
 			}
 		},
 		{
-			[](Player* p1, Player* p2) -> float{
+			[](auto p1, auto p2) -> float {
 				return Game::getEnvironment()->getDiffOfCenters(CenterType::ARMY, p2->getId(), CenterType::BUILDING,
 				                                                p2->getId(), 0.f);
 			}
 		},
 	};
 
-	static inline AiPlayerMetric aiWhereAttack[] = {//Player* p1, Player* p2
-		{[](Player* p1, Player* p2) -> float{ return p1->getPossession()->getAttackSum(); }, 1000},
+	static inline AiPlayerMetric aiWhereAttack[] = { //Player* p1, Player* p2
+		{[](auto p1, auto p2) -> float { return p1->getPossession()->getAttackSum(); }, 1000},
 		{
-			[](Player* p1, Player* p2) -> float{
+			[](auto p1, auto p2) -> float {
 				return Game::getEnvironment()->getDiffOfCenters(CenterType::ARMY, p1->getId(), CenterType::ECON,
 				                                                p2->getId(), 1.f);
 			}
 		},
 		{
-			[](Player* p1, Player* p2) -> float{
+			[](auto p1, auto p2) -> float {
 				return Game::getEnvironment()->getDiffOfCenters(CenterType::ARMY, p1->getId(), CenterType::BUILDING,
 				                                                p2->getId(), 1.f);
 			}
 		},
 		{
-			[](Player* p1, Player* p2) -> float{
+			[](auto p1, auto p2) -> float {
 				return Game::getEnvironment()->getDiffOfCenters(CenterType::ARMY, p1->getId(), CenterType::ARMY,
 				                                                p2->getId(), 1.f);
 			}
 		},
 	};
 
-	static inline AiPlayerMetric aiWhereDefend[] = {
-		{[](Player* p1, Player* p2) -> float{ return p1->getPossession()->getAttackSum(); }, 1000},
-		{[](Player* p1, Player* p2) -> float{ return p1->getPossession()->getDefenceAttackSum(); }, 100},
+	static inline AiPlayerMetric aiWhereDefend[] = { //Player* p1, Player* p2
+		{[](auto p1, auto p2) -> float { return p1->getPossession()->getAttackSum(); }, 1000},
+		{[](auto p1, auto p2) -> float { return p1->getPossession()->getDefenceAttackSum(); }, 100},
 		{
-			[](Player* p1, Player* p2) -> float{
+			[](auto p1, auto p2) -> float {
 				return Game::getEnvironment()->getDiffOfCenters(CenterType::ARMY, p1->getId(), CenterType::ECON,
 				                                                p1->getId(), 0.f);
 			}
 		},
 		{
-			[](Player* p1, Player* p2) -> float{
+			[](auto p1, auto p2) -> float {
 				return Game::getEnvironment()->getDiffOfCenters(CenterType::ARMY, p1->getId(), CenterType::BUILDING,
 				                                                p1->getId(), 0.f);
 			}
 		},
 		{
-			[](Player* p1, Player* p2) -> float{
+			[](auto p1, auto p2) -> float {
 				return Game::getEnvironment()->getDiffOfCenters(CenterType::ARMY, p1->getId(), CenterType::ARMY,
 				                                                p2->getId(), 1.f);
 			}
